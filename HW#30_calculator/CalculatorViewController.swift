1108b4d21718f1f6105c63b454499438c2622322
Daweihao84 <dwsamurai84@gmail.com>
2023-07-20 00:21:13 +0800

    //

--- HW#30_calculator/CalculatorViewController.swift
+++ HW#30_calculator/CalculatorViewController.swift
@@ -4,40 +4,6 @@
 //
 //  Created by Dawei Hao on 2023/6/30.
 //
-/*
- Practice Purpose
- 1.先乘除後加減，比方 3 + 4 * 5 會得到 35。
- 2.加入負數
- 3.加入小數點
- 4.點擊UIButton，按鈕顏色會變色(參考Iphone)
- 5.加入Auto Layout
- 6.當數字大於0時，btnAC轉成文字btnClear
-*/
-
-/*
- tag's value
- 
- sender.tag == 1 //num 0
- sender.tag == 2 //num 1
- sender.tag == 3 //num 2
- sender.tag == 4 //num 3
- sender.tag == 5 //num 4
- sender.tag == 6 //num 5
- sender.tag == 7 //num 6
- sender.tag == 8 //num 7
- sender.tag == 9 //num 8
- sender.tag == 10 // num 9
- sender.tag == 11 // btnAllClear
- sender.tag == 12 // btnPlusForwardslashMinus
- 
- sender.tag == 13 // percent
- sender.tag == 14 // Divide
- sender.tag == 15 // multiply
- sender.tag == 16 // minus
- sender.tag == 17 // plus
- sender.tag == 18 // equal
- sender.tag == 19 // Dot
- */
 
 import UIKit
 
@@ -46,14 +12,32 @@ class CalculatorViewController: UIViewController {
     //數字按鈕的Outlet Collection
     @IBOutlet var numbersBtn: [UIButton]!
     @IBOutlet weak var btnAllClear: UIButton!
+
     //加減乘除按鈕的Outlet Collection
     @IBOutlet var operatorsBtn: [UIButton]!
     @IBOutlet weak var btnPercentage: UIButton!
     @IBOutlet weak var btnPlusForwardslashMinus: UIButton!
     @IBOutlet weak var numberLabel: UILabel!
+
+    //operator outlet
     @IBOutlet weak var dotBtn: UIButton!
     @IBOutlet weak var equalBtn: UIButton!
+    @IBOutlet weak var divideBtn: UIButton!
+    @IBOutlet weak var multiplyBtn: UIButton!
+    @IBOutlet weak var minusBtn: UIButton!
+    @IBOutlet weak var plusBtn: UIButton!
 
+    //numbers outlet
+    @IBOutlet weak var zeroBtn: UIButton!
+    @IBOutlet weak var oneBtn: UIButton!
+    @IBOutlet weak var twoBtn: UIButton!
+    @IBOutlet weak var threeBtn: UIButton!
+    @IBOutlet weak var fourBtn: UIButton!
+    @IBOutlet weak var fiveBtn: UIButton!
+    @IBOutlet weak var sixBtn: UIButton!
+    @IBOutlet weak var sevenBtn: UIButton!
+    @IBOutlet weak var eightBtn: UIButton!
+    @IBOutlet weak var nineBtn: UIButton!
 
     //呈現計算的狀態>>> 是或不是
     var performingMath: Bool = false
@@ -64,87 +48,82 @@ class CalculatorViewController: UIViewController {
     //小數點
     var decimals: Bool = false
 
+    //numbersBtn.backgroundColor = UIColor.systemGray5
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //初始設定numberLabel.text的數字為0
+        //set up numberLabel.text is "0"
         numberLabel.text = "0"
-        //調整文字至符合寬度
+        //let text could be fix to width
         numberLabel.adjustsFontSizeToFitWidth = true
-
-
+        //update the btnTargetAction
+        updateBtnTargetAction ()
     }
 
 
     @IBAction func numberButtonTapped(_ sender: UIButton) {
-
-        let numberTag = sender.tag
+        let numberTag = sender.tag - 1
         //當計算的數字超過第一個是字數時
         if performingMath == true {
-            numberLabel.text = String(numberTag - 1)
+            numberLabel.text = String(numberTag)
             performingMath = false
 
         } else {
             //判斷第一位數是否為"0"
             if numberLabel.text != "0" {
-                numberLabel.text! += String(numberTag - 1)
+                numberLabel.text! += String(numberTag)
             } else {
                 numberLabel.text = ""
-                numberLabel.text! += String(numberTag - 1)
+                numberLabel.text! += String(numberTag)
             }
-        } ; print(numberTag-1)
+        } ; print(numberTag)
     }
-
-
 
     @IBAction func operatorsBtnTapped(_ sender: UIButton) {
-        //布林值decimals ＝true時代表為小數點
-        var doubleNum: String
-        if performingMath == true {
-            doubleNum = ".0"
-        } else {
-            doubleNum = ""
-        }
 
         if let inputNum = numberLabel.text {
+            let operatorSign: String
             switch sender.tag {
                 case 14:
-                    equation = inputNum + doubleNum + "/"
+                    operatorSign = "/"
                     print("divide")
                 case 15:
-                    equation = inputNum + doubleNum + "*"
+                    operatorSign = "*"
                     print("multiply")
                 case 16:
-                    equation = inputNum + "-" + doubleNum
+                    operatorSign = "-"
                     print("minus")
                 case 17:
-                    equation = inputNum + "+" + doubleNum
+                    operatorSign = "+"
                     print("plus")
                 default:
-                    break
+                    return
             }
-        } ;print(equation)
+            equation = inputNum + operatorSign
+            numberLabel.text = ""
+            print("happening")
+        }
     }
-
 
     @IBAction func equalBtnTapped(_ sender: UIButton) {
 
-        if let inputNum = numberLabel.text {
+        if let inputNum = numberLabel.text, !inputNum.isEmpty {
             equation += inputNum
             print(equation)
+        }
 
+        let expression = NSExpression(format: equation)
+        if let calculateResult = expression.expressionValue(with: nil, context: nil) as? Double {
+            print(calculateResult)
+            if calculateResult.truncatingRemainder(dividingBy: 1) == 0 {
+                numberLabel.text = String(format: "%.0f", calculateResult)
+                print(calculateResult)
+            } else {
+                numberLabel.text = String(format: "%.6f", calculateResult)
+            }
         }
-            let expression = NSExpression(format: equation)
-            if let calculateResult = expression.expressionValue(with: nil, context: nil) as? Double {
-                        print(calculateResult)
-                        if calculateResult.truncatingRemainder(dividingBy: 1) == 0 {
-                            numberLabel.text = String(calculateResult.floor(toInteger: 1))
-                            print(calculateResult)
-                        } else {
-                            numberLabel.text = String(calculateResult.rounding(toDecimal: 6))
-                }
-            } 
-        }
+    }
 
     @IBAction func dotBtnTapped(_ sender: UIButton) {
         btnTappedCount += 1
@@ -162,6 +141,8 @@ class CalculatorViewController: UIViewController {
         numberLabel.text = "0"
         equation = ""
         btnTappedCount = 0
+        performingMath = false
+        decimals = false
         print("clearBtnTapped")
     }
 
@@ -190,6 +171,7 @@ class CalculatorViewController: UIViewController {
                             numberLabel.text = "\(abs(doubleResultNum))"
                             print("3")
                             print(doubleResultNum)
+
                         } else {
                             //將numberLabel.text使用abs()再加上Int(), 將資料轉換成"\(abs(Int(doubleResultNum)))"
                             numberLabel.text = "\(abs(Int(doubleResultNum)))"
@@ -199,11 +181,10 @@ class CalculatorViewController: UIViewController {
                     }
                 }
             }
-        }
+        }; print("btnPlusForwardslashMinusBtnTapped")
     }
 
     @IBAction func percentageBtnTapped(_ sender: UIButton) {
-
         let number = Double(numberLabel.text!) ?? 0.0
         let percentageNum = number / 100
 
@@ -215,9 +196,230 @@ class CalculatorViewController: UIViewController {
         if let formattedPercentageNum = numberFormatter.string(from: NSNumber(value: percentageNum)) {
             numberLabel.text = formattedPercentageNum
             print(formattedPercentageNum)
+        }; print("percentageBtnTapped")
+    }
+
+    func updateBtnTargetAction () {
+        //allClear
+        btnAllClear.addTarget(self, action: #selector(updateClearUI), for: .touchDown)
+        //plusForwardslashMinus
+        btnPlusForwardslashMinus.addTarget(self, action: #selector(updateBtnPlusForwardslashMinusUI), for: .touchDown)
+        //percentageBtn
+        btnPercentage.addTarget(self, action: #selector(updatePercentageUI), for: .touchDown)
+        //divideBtn
+        divideBtn.addTarget(self, action: #selector(updateDivideBtnUI), for: .touchDown)
+        //multiplyBtn
+        multiplyBtn.addTarget(self, action: #selector(updateMultiplyBtnUI), for: .touchDown)
+        //minusBtn
+        minusBtn.addTarget(self, action: #selector(updateMinusBtnUI), for: .touchDown)
+        //plusBtn
+        plusBtn.addTarget(self, action: #selector(updatePlusBtnUI), for: .touchDown)
+        //equalBtn
+        equalBtn.addTarget(self, action: #selector(updateEqualBtnUI), for: .touchDown)
+        // number 0
+        zeroBtn.addTarget(self, action: #selector(updateZeroBtnUI), for: .touchDown)
+        // number 1
+        oneBtn.addTarget(self, action: #selector(updateOneBtnUI), for: .touchDown)
+        // number 2
+        twoBtn.addTarget(self, action: #selector(updateTwoBtnUI), for: .touchDown)
+        // number 3
+        threeBtn.addTarget(self, action: #selector(updateThreeBtnUI), for: .touchDown)
+        // number 4
+        fourBtn.addTarget(self, action: #selector(updateFourBtnUI), for: .touchDown)
+        // number 5
+        fiveBtn.addTarget(self, action: #selector(updateFiveBtnUI), for: .touchDown)
+        // number 6
+        sixBtn.addTarget(self, action: #selector(updateSixBtnUI), for: .touchDown)
+        // number 7
+        sevenBtn.addTarget(self, action: #selector(updateSevenBtnUI), for: .touchDown)
+        // number 8
+        eightBtn.addTarget(self, action: #selector(updateEightBtnUI), for: .touchDown)
+        // number 9
+        nineBtn.addTarget(self, action: #selector(updateNineBtnUI), for: .touchDown)
+        // dotBtn
+        dotBtn.addTarget(self, action: #selector(updateDotBtnUI), for: .touchDown)
+    }
+
+    //Build the btn Clear's color changing function
+    @objc func updateClearUI () {
+        btnAllClear.backgroundColor = UIColor(red: 217/255, green: 217/255, blue: 217/255, alpha: 1)
+        if btnAllClear.backgroundColor == UIColor(red: 217/255, green: 217/255, blue: 217/255, alpha: 1) {
+            UIView.animate(withDuration: 0.3) {
+                self.btnAllClear.backgroundColor = UIColor.lightGray
+            }
+        }
+    }
+    //Build the btn PlusForwardslashMinus's color changing function
+    @objc func updateBtnPlusForwardslashMinusUI () {
+        btnPlusForwardslashMinus.backgroundColor = UIColor(red: 217/255, green: 217/255, blue: 217/255, alpha: 1)
+        if btnPlusForwardslashMinus.backgroundColor == UIColor(red: 217/255, green: 217/255, blue: 217/255, alpha: 1) {
+            UIView.animate(withDuration: 0.3) {
+                self.btnPlusForwardslashMinus.backgroundColor = UIColor.lightGray
+            }
+        }
+    }
+    //Build the btn Percentage's color changing function
+    @objc func updatePercentageUI () {
+        btnPercentage.backgroundColor = UIColor(red: 217/255, green: 217/255, blue: 217/255, alpha: 1)
+        if btnPercentage.backgroundColor == UIColor(red: 217/255, green: 217/255, blue: 217/255, alpha: 1) {
+            UIView.animate(withDuration: 0.3) {
+                self.btnPercentage.backgroundColor = UIColor.lightGray
+            }
+        }
+    }
+    //建立按鈕Divide的變色功能
+    @objc func updateDivideBtnUI () {
+        divideBtn.backgroundColor = UIColor.white
+        divideBtn.tintColor = UIColor.systemOrange
+        if divideBtn.backgroundColor == UIColor.white {
+            UIView.animate(withDuration: 0.3) {
+                self.divideBtn.backgroundColor = UIColor.systemOrange
+                self.divideBtn.tintColor = UIColor.white
+            }
+        }
+    }
+    //建立按鈕Multiply的變色功能
+    @objc func updateMultiplyBtnUI () {
+            multiplyBtn.backgroundColor = UIColor.white
+            multiplyBtn.tintColor = UIColor.systemOrange
+            if multiplyBtn.backgroundColor == UIColor.white {
+                UIView.animate(withDuration: 0.3) {
+                    self.multiplyBtn.backgroundColor = UIColor.systemOrange
+                    self.multiplyBtn.tintColor = UIColor.white
+            }
+        }
+    }
+    //建立按鈕Minus的變色功能
+    @objc func updateMinusBtnUI () {
+        minusBtn.backgroundColor = UIColor.white
+        minusBtn.tintColor = UIColor.systemOrange
+        if minusBtn.backgroundColor == UIColor.white {
+            UIView.animate(withDuration: 0.3) {
+                self.minusBtn.backgroundColor = UIColor.systemOrange
+                self.minusBtn.tintColor = UIColor.white
+            }
+        }
+    }
+    //建立按鈕Plus的變色功能
+    @objc func updatePlusBtnUI () {
+        plusBtn.backgroundColor = UIColor.white
+        plusBtn.tintColor = UIColor.systemOrange
+        if plusBtn.backgroundColor == UIColor.white {
+            UIView.animate(withDuration: 0.3) {
+                self.plusBtn.backgroundColor = UIColor.systemOrange
+                self.plusBtn.tintColor = UIColor.white
+            }
+        }
+    }
+    //建立按鈕equal的變色功能
+    @objc func updateEqualBtnUI () {
+        equalBtn.backgroundColor = UIColor.white
+        equalBtn.tintColor = UIColor.systemOrange
+        if equalBtn.backgroundColor == UIColor.white {
+            UIView.animate(withDuration: 0.3) {
+                self.equalBtn.backgroundColor = UIColor.systemOrange
+                self.equalBtn.tintColor = UIColor.white
+            }
+        }
+    }
+    //建立按鈕0的變色功能
+    @objc func updateZeroBtnUI () {
+        zeroBtn.backgroundColor = UIColor.lightGray
+        if zeroBtn.backgroundColor == UIColor.lightGray {
+            UIView.animate(withDuration: 0.3) {
+                self.zeroBtn.backgroundColor = UIColor.darkGray
+            }
+        }
+    }
+    //建立按鈕1的變色功能
+    @objc func updateOneBtnUI () {
+        oneBtn.backgroundColor = UIColor.lightGray
+        if oneBtn.backgroundColor == UIColor.lightGray {
+            UIView.animate(withDuration: 0.3) {
+                self.oneBtn.backgroundColor = UIColor.darkGray
+            }
+        }
+    }
+    //建立按鈕2的變色功能
+    @objc func updateTwoBtnUI () {
+        twoBtn.backgroundColor = UIColor.lightGray
+        if twoBtn.backgroundColor == UIColor.lightGray {
+            UIView.animate(withDuration: 0.3) {
+                self.twoBtn.backgroundColor = UIColor.darkGray
+            }
+        }
+    }
+    //建立按鈕3的變色功能
+    @objc func updateThreeBtnUI () {
+        threeBtn.backgroundColor = UIColor.lightGray
+        if threeBtn.backgroundColor == UIColor.lightGray {
+            UIView.animate(withDuration: 0.3) {
+                self.threeBtn.backgroundColor = UIColor.darkGray
+            }
+        }
+    }
+    //建立按鈕4的變色功能
+    @objc func updateFourBtnUI () {
+        fourBtn.backgroundColor = UIColor.lightGray
+        if fourBtn.backgroundColor == UIColor.lightGray {
+            UIView.animate(withDuration: 0.3) {
+                self.fourBtn.backgroundColor = UIColor.darkGray
+            }
+        }
+    }
+    //建立按鈕5的變色功能
+    @objc func updateFiveBtnUI () {
+        fiveBtn.backgroundColor = UIColor.lightGray
+        if fiveBtn.backgroundColor == UIColor.lightGray {
+            UIView.animate(withDuration: 0.3) {
+                self.fiveBtn.backgroundColor = UIColor.darkGray
+            }
+        }
+    }
+    //建立按鈕6的變色功能
+    @objc func updateSixBtnUI () {
+        sixBtn.backgroundColor = UIColor.lightGray
+        if sixBtn.backgroundColor == UIColor.lightGray {
+            UIView.animate(withDuration: 0.3) {
+                self.sixBtn.backgroundColor = UIColor.darkGray
+            }
+        }
+    }
+    //建立按鈕7的變色功能
+    @objc func updateSevenBtnUI () {
+        sevenBtn.backgroundColor = UIColor.lightGray
+        if sevenBtn.backgroundColor == UIColor.lightGray {
+            UIView.animate(withDuration: 0.3) {
+                self.sevenBtn.backgroundColor = UIColor.darkGray
+            }
+        }
+    }
+    //建立按鈕8的變色功能
+    @objc func updateEightBtnUI () {
+        eightBtn.backgroundColor = UIColor.lightGray
+        if eightBtn.backgroundColor == UIColor.lightGray {
+            UIView.animate(withDuration: 0.3) {
+                self.eightBtn.backgroundColor = UIColor.darkGray
+            }
+        }
+    }
+    //建立按鈕9的變色功能
+    @objc func updateNineBtnUI () {
+        nineBtn.backgroundColor = UIColor.lightGray
+        if nineBtn.backgroundColor == UIColor.lightGray {
+            UIView.animate(withDuration: 0.3) {
+                self.nineBtn.backgroundColor = UIColor.darkGray
+            }
+        }
+    }
+    //建立按鈕dot的變色功能
+    @objc func updateDotBtnUI () {
+        dotBtn.backgroundColor = UIColor.lightGray
+        if dotBtn.backgroundColor == UIColor.lightGray {
+            UIView.animate(withDuration: 0.3) {
+                self.dotBtn.backgroundColor = UIColor.darkGray
+            }
         }
     }
 
 }
-
-
---
+++ extension_markdown.swift
@@ -0,0 +1,45 @@
+//
+//  extension_UI.swift
+//  HW#30_calculator
+//
+//  Created by Dawei Hao on 2023/7/18.
+//
+
+import Foundation
+
+/*
+ Practice Purpose
+ 1.先乘除後加減，比方 3 + 4 * 5 會得到 35。
+ 2.加入負數
+ 3.加入小數點
+ 4.點擊UIButton，按鈕顏色會變色(參考Iphone)
+ 5.加入Auto Layout
+ 6.當數字大於0時，btnAC轉成文字btnClear
+*/
+
+/*
+ tag's value
+
+ sender.tag == 1 //num 0
+ sender.tag == 2 //num 1
+ sender.tag == 3 //num 2
+ sender.tag == 4 //num 3
+ sender.tag == 5 //num 4
+ sender.tag == 6 //num 5
+ sender.tag == 7 //num 6
+ sender.tag == 8 //num 7
+ sender.tag == 9 //num 8
+ sender.tag == 10 // num 9
+ sender.tag == 11 // btnAllClear
+ sender.tag == 12 // btnPlusForwardslashMinus
+
+ sender.tag == 13 // percent
+ sender.tag == 14 // Divide
+ sender.tag == 15 // multiply
+ sender.tag == 16 // minus
+ sender.tag == 17 // plus
+ sender.tag == 18 // equal
+ sender.tag == 19 // Dot
+ */
+
+
